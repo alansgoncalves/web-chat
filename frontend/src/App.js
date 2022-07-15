f8523d692cca2a311c6422a3d59b6e51ef29848d
alansgoncalves <vekio.soft@gmail.com>
2022-07-15 09:48:06 -0300

    feat: create Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <Button colorScheme="blue">Button</Button>
+      <Route path="/" component={HomePage} />
     </div>
   );
 }
