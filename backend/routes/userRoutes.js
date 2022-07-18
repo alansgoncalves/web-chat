7910c843ddad589d51b875941c65aade3c9212c9
alansgoncalves <vekio.soft@gmail.com>
2022-07-18 18:02:36 -0300

    fix: userModel and userRoutes config

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
-const express = require("expres");
+const express = require("express");
+const { registerUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-// router.route('/').post(resgisterUser)
+router.route("/").post(registerUser);
 // router.route("/login", authUser);
 
 module.exports = router;
