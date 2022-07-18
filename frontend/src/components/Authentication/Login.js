54a569e34e6599cbad60776115019c9c13e3528a
alansgoncalves <vekio.soft@gmail.com>
2022-07-18 13:22:11 -0300

    feat: create login form

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,7 +1,65 @@
-import React from "react";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { VStack } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Entre</div>;
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleClick = () => setShow(!show);
+
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing="5px" color="black">
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Senha</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Esconder" : "Exibir"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        color="white"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Entrar
+      </Button>
+      <Button
+        variant="solid"
+        colorScheme="red"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={() => {
+          setEmail("guest@example.com");
+          setPassword("123456");
+        }}
+      >
+        Obtenha suas credencias de usuário
+      </Button>
+    </VStack>
+  );
 };
 
 export default Login;
