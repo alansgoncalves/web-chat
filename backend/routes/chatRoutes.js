229259e85a1ecfaaea20d12ddfbb6c099a81a17f
alansgoncalves <vekio.soft@gmail.com>
2022-07-22 12:26:34 -0300

    feat: create addToGroup controller

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,6 +4,7 @@ const {
   fetchChats,
   createGroupChat,
   renameGroup,
+  addToGroup,
 } = require("../controllers/chatControllers");
 const router = express.Router();
 const { protect } = require("../midleware/authMiddleware");
@@ -13,6 +14,6 @@ router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
 router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
