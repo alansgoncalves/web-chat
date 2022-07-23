068ea0291c5ffece6adce6a0e832e5c94d7ce8b0
alansgoncalves <vekio.soft@gmail.com>
2022-07-23 16:49:05 -0300

    chore: install fontAwsomeIcons

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,31 @@
-import React from "react";
+import { Box } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/tooltip";
+import { Button } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SideDrawer = () => {
-  return <div>Side Drawer</div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
+
+  return (
+    <>
+      <Box>
+        <Tooltip
+          label="Search for users to chat"
+          hasArrow
+          placement="bottom-end"
+        >
+          <Button variant="ghost">
+            <FontAwesomeIcon icon={faSearch} />
+          </Button>
+        </Tooltip>
+      </Box>
+    </>
+  );
 };
 
 export default SideDrawer;
