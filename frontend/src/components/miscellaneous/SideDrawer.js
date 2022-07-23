8778384447b1331ad0eb1f326f54aead94a223c8
alansgoncalves <vekio.soft@gmail.com>
2022-07-23 18:12:35 -0300

    feat: create header chat page

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,9 +1,18 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Text } from "@chakra-ui/layout";
 import { Tooltip } from "@chakra-ui/tooltip";
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  Avatar,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { ChatState } from "../../Context/ChatProvider";
 
 const SideDrawer = () => {
   const [search, setSearch] = useState("");
@@ -11,9 +20,19 @@ const SideDrawer = () => {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState(false);
 
+  const { user } = ChatState();
+
   return (
     <>
-      <Box>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
         <Tooltip
           label="Search for users to chat"
           hasArrow
@@ -21,8 +40,35 @@ const SideDrawer = () => {
         >
           <Button variant="ghost">
             <FontAwesomeIcon icon={faSearch} />
+            <Text display={{ base: "none", md: "flex" }} px="4">
+              Search user
+            </Text>
           </Button>
         </Tooltip>
+        <Text fontSize="2xl" fontFamily="Work sans">
+          web-ch@t
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <MenuItem>My Profile</MenuItem>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
       </Box>
     </>
   );
