202fdb60ecf7d5dbd16fc5686d46c55152d8e709
alansgoncalves <vekio.soft@gmail.com>
2022-07-25 21:30:06 -0300

    feat: add functionality to render chats

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,7 +1,11 @@
-import { useToast } from "@chakra-ui/react";
+import { useToast, Button } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { ChatState } from "../Context/ChatProvider";
+import { Box, Stack, Text } from "@chakra-ui/layout";
 import axios from "axios";
+import { AddIcon } from "@chakra-ui/icons";
+import ChatLoading from "./ChatLoading";
+import { getSender } from "../config/ChatLogics";
 
 const MyChats = () => {
   const [loggedUser, setLoggedUser] = useState();
@@ -36,7 +40,64 @@ const MyChats = () => {
     fetchChats();
   }, []);
 
-  return <div>My Chats</div>;
+  return (
+    <Box
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir="column"
+      alignItems="center"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "31%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "28px", md: "30px" }}
+        fontFamily="Work sans"
+        display="flex"
+        w="100%"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        My Chats
+        <Button
+          display="flex"
+          fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+          rightIcon={<AddIcon />}
+        >
+          New Group Chat
+        </Button>
+      </Box>
+      <Box
+        display="flex"
+        flexDir="column"
+        p={3}
+        bg="#F8F8F8"
+        w="100%"
+        h="100%"
+        borderRadius="lg"
+        overflowY="hidden"
+      >
+        {chats ? (
+          <Stack overflowY="scroll">
+            {chats.map((chat) => (
+              <Box key={chat._id}>
+                <Text>
+                  {!chat.isGroupChat
+                    ? getSender(loggedUser, chat.users)
+                    : chat.chatName}
+                </Text>
+              </Box>
+            ))}
+          </Stack>
+        ) : (
+          <ChatLoading />
+        )}
+      </Box>
+    </Box>
+  );
 };
 
 export default MyChats;
