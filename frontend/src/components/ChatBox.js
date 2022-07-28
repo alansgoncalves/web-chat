26d7d845db1957d18531597e30da7432f54a5977
alansgoncalves <vekio.soft@gmail.com>
2022-07-28 16:40:51 -0300

    feat: add ChatBox screen

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/react";
 
 const ChatBox = () => {
-  return <div>Chat Box</div>;
+  const { selectedChat } = ChatState();
+  return (
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      {/* <SingleChat /> */}
+    </Box>
+  );
 };
 
 export default ChatBox;
