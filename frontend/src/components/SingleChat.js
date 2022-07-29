919d8402ab2ba7c5e1ab061669ba67b0d392260a
alansgoncalves <vekio.soft@gmail.com>
2022-07-29 10:59:36 -0300

    feat: create handleRename functionality

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -4,6 +4,7 @@ import { ChatState } from "../Context/ChatProvider";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import ProfileModal from "./miscellaneous/ProfileModal";
+import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   const { user, selectedChat, setSelectedChat } = ChatState();
@@ -35,10 +36,10 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             ) : (
               <>
                 {selectedChat.chatName.toUpperCase()}
-                {/* <UpdateGroupChatModal
+                <UpdateGroupChatModal
                   fetchAgain={fetchAgain}
                   setFetchAgain={setFetchAgain}
-                /> */}
+                />
               </>
             )}
           </Text>
