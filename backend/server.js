83646f9fa363bfab0d54c5d6b048eb392f8acf1e
alansgoncalves <vekio.soft@gmail.com>
2022-08-02 15:30:54 -0300

    feat: add socket.off to clean up socket

--- backend/server.js
+++ backend/server.js
@@ -63,4 +63,8 @@ io.on("connection", (socket) => {
       socket.in(user._id).emit("message received", newMessageRecieved);
     });
   });
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
