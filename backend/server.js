ff8b9d86d53788b52bc4d88667249f8d7ea4b2a8
alansgoncalves <vekio.soft@gmail.com>
2022-08-03 11:18:25 -0300

    make it better

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const { notFound, errorHandler } = require("./midleware/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
 
 dotenv.config();
 
@@ -14,13 +15,30 @@ const app = express();
 
 app.use(express.json()); //to accept the json data
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------------Deployment------------------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------Deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
@@ -32,7 +50,7 @@ const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3001/",
   },
 });
 
